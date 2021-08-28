840bde1a96410b8fa60838ef1b32012590b31269
bekushka <bekzod.tursunoff@gmail.com>
2021-08-28 19:49:54 +0500

    beautify input box, functionality and create custom scope

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(username: "Bekzod", password: "password")
+User.create(username: "Farangiz", password: "password")
+User.create(username: "Shahzod", password: "password")
+User.create(username: "Xonzoda", password: "password")
+User.create(username: "Malika", password: "password")
+User.create(username: "Khasan", password: "password")
+
